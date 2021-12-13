42018a33b7e1cef8dfa1a426dff7cd0b9b4211db
zqm2021 <zqm2021@protonmail.com>
2021-12-13 16:12:15 +0800

    sync

--- jx_sign.js
+++ jx_sign.js
@@ -64,7 +64,7 @@ if ($.isNode()) {
       UA = `jdpingou;iPhone;4.13.0;14.4.2;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100609;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148`
       UAInfo[$.UserName] = UA
       if (isLoginInfo[$.UserName] === false) {
-      
+
       } else {
         if (!isLoginInfo[$.UserName]) {
           await TotalBean();
@@ -100,7 +100,7 @@ if ($.isNode()) {
       $.black = false
       $.canHelp = true
       if (isLoginInfo[$.UserName] === false) {
-      
+
       } else {
         if (!isLoginInfo[$.UserName]) {
           await TotalBean();
@@ -506,7 +506,7 @@ async function requestAlgo() {
       "expandParams": ""
     })
   }
-  new Promise(async resolve => {
+  return new Promise(async resolve => {
     $.post(options, (err, resp, data) => {
       try {
         if (err) {
